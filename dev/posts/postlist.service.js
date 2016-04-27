400e317668ff61808c37c695dffca9201da15704
MJG <MJG@MacBook-Pro.local>
2016-04-27 15:33:48 -0400

    Fixed the name of blog.module.js

--- dev/posts/postlist.service.js
+++ dev/posts/postlist.service.js
@@ -13,13 +13,57 @@
     {id: 5555, title: 'A Song of Lice and Tires', author: 'martin', category: 'politics'},
   ];
 
+  postListFactory.$inject = ['$http'];
+
 /**
  * This factory should be able to retrieve a list of blog posts for a given
  * category or author or else all posts.
  * It should return that list as an array of objects.
  */
-  function postListFactory() {
-    return storyList;
+  function postListFactory($http) {
+
+    var apiURL = 'https://tiy-blog-api.herokuapp.com/api';
+
+    return {
+      getCategoryID: getCategoryID,
+      getAllPosts: getAllPosts,
+      getPostsByCategory: getPostsByCategoryID,
+      getPostsByAuthor: getPostsByAuthor
+    };
+
+    function getCategoryID(category) {
+      return $http({
+        method: 'GET',
+        url: apiURL + '/Categories',
+        data: {
+          filter: {include:["posts"]}
+        }
+      }).then(function successGetCategory(response) {
+        console.log('this is working');
+        console.log(response);
+      }
+
+      );
+    }
+
+    function getPostsByCategoryID(categoryID) {
+      return $http({
+        method: 'GET',
+        url: apiURL + '/Posts',
+        data: {
+          filter: {include:["author","category"]}
+        }
+      });
+    }
+
+    function getPostsByAuthor() {
+
+    }
+
+    function getAllPosts() {
+
+    }
+
     // TODO: This should do some logic to figure out what subset of the list
     // was asked for. It should also get real data from a server, not fake data
     // from above.
