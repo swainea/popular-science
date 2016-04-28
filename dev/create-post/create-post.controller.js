719b700b3bc8e71d9b5946d5de082a02a3500677
Liz Swain <swainea@gmail.com>
2016-04-28 15:43:32 -0400

    blog post objects are being created

--- dev/create-post/create-post.controller.js
+++ dev/create-post/create-post.controller.js
@@ -9,14 +9,16 @@
 
   function CreatePostController (CreatePostService){
     this.blogPost = {
-      "title": "",
-      "content": "",
-      "categoryId": "",
-      "authorId": "5722369d84c2fd11003f9f2b" 
+      title: "",
+      content: "",
+      categoryId: "571e6e9362e24e1100c9e4c2",
+      userId: "5722369d84c2fd11003f9f2b"
     };
     this.newPost = function newPost (){
-      //this function needs to post a new post to the internet and send uf tyo a view that shows that this happened
+      //this function needs to post a new post to the internet and send us to a view that shows that this happened
       CreatePostService.submitPost(this.blogPost);
+      console.log("inside of newPost function");
+
     };
   }
 }());
--- dev/create-post/create-post.service.js
+++ dev/create-post/create-post.service.js
@@ -14,18 +14,21 @@
     };
 
     function submitPost (blogPost){
+      console.log(blogPost);
       return $http ({
         method:'POST',
         url: "https://tiy-blog-api.herokuapp.com/api/Posts",
         data: blogPost,
         headers: {
-          Authorization: {
-            id: "cStlRZdmrEnDqJr8V80SBlddBWlrBtj1N3Bbc7SJC4w1aE28MMyW2hxbKh7M3vbN",
-          }
+          Authorization:  "lYldEKUsuEELUiFwFcRRNm1o1YjsGSsCAUwWzTmgmtdNfYj2p9Dwi9FHEtwdCSAW"
+
         }
       }).then (function onSuccess(response){
+        console.log("inside of onSuccess function", response);
+      }, function error(response) {
         console.log(response);
-      });
+      }
+    );
     }
   }
 
