e43860713f97cea5de436d1a112e0309646e0690
Liz Swain <swainea@gmail.com>
2016-04-28 10:34:16 -0400

    fixed alignment on module

--- dev/about/about.controller.js
+++ dev/about/about.controller.js
@@ -8,6 +8,6 @@
   AboutController.$inject = ["$state"];
 
   function AboutController($state){
-    
+
   }
 }());
--- dev/app/blog.module.js
+++ dev/app/blog.module.js
@@ -2,8 +2,8 @@
   'use strict';
 
   angular
-    .module('blog', ['ui.router'])
-    .config(blogConfig);
+  .module('blog', ['ui.router'])
+  .config(blogConfig);
 
   blogConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
@@ -15,10 +15,6 @@
       url: '/'
       // templateUrl: 'welcome/welcome.html'
     })
-    .state('about', {
-      url: '/about'
-      // templateUrl: 'welcome/welcome.html'
-    })
     .state('categories', {
       url: '/categories'
       // templateUrl: 'welcome/welcome.html'
@@ -27,29 +23,24 @@
       url: '/login'
       // templateUrl: 'welcome/welcome.html'
     })
-      .state('allStories', {
-        url: '/allStories',
-        templateURL: ''
-        // TODO: create a template for 'allStories' and include its URL here
-      })
-      .state('categoryStories', {
-        url: '/:name',
-        templateURL: ''
-        // TODO: create a template for 'categoryStories and include its URL here'
-      })
-      .state('about', {
-        url: '/:about',
-        templateURL: '',
-        controller: 'AboutController',
-        controllerAs: 'about'
-        // TODO: create a template for "about" and include its URL here'
-      });
+    .state('allStories', {
+      url: '/allStories',
+      templateURL: ''
+      // TODO: create a template for 'allStories' and include its URL here
+    })
+    .state('categoryStories', {
+      url: '/:name',
+      templateURL: ''
+      // TODO: create a template for 'categoryStories and include its URL here'
+    })
+    .state('about', {
+      url: '/:about',
+      templateURL: '',
+      controller: 'AboutController',
+      controllerAs: 'about'
+      // TODO: create a template for "about" and include its URL here'
+    });
 
-
-<<<<<<< HEAD:dev/app/blog.controller.js
-
-=======
->>>>>>> upstream/master:dev/app/blog.module.js
   }
 
 })();
