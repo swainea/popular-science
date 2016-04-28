da42bc3a39a9813243841094908f4575532b5107
Liz Swain <swainea@gmail.com>
2016-04-28 10:32:20 -0400

    constructing post service request

---
+++ dev/create-post/create-post.service.js
@@ -0,0 +1,17 @@
+// (function() {
+//   'use strict';
+//
+//   angular
+//     .module('blog')
+//     .factory( 'CreatePostService', CreatePostService );
+//
+//   CreatePostService.$inject = ['$http'];
+//
+//   function CreatePostService ($http){
+//     if ( );
+//   } return $http ({
+//       method:'POST',
+//       url:
+//   });
+//
+// }());
