8caedc0e9cd29d09dd140b5e685579e65b5f3b6a
MJG <MJG@MacBook-Pro.local>
2016-04-28 21:27:17 -0400

    Cleaned up the blog.module.js file a bit

--- dev/app/blog.module.js
+++ dev/app/blog.module.js
@@ -8,6 +8,7 @@
   blogConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
   function blogConfig($stateProvider, $urlRouterProvider) {
+    
     $urlRouterProvider.otherwise('/');
 
     $stateProvider
@@ -16,11 +17,9 @@
     })
     .state('categories', {
       url: '/categories'
-      // templateUrl: 'welcome/welcome.html'
     })
     .state('login', {
       url: '/login'
-      // templateUrl: 'welcome/welcome.html'
     })
     .state('allPosts', {
       url: '/allPosts',
@@ -35,8 +34,7 @@
       controllerAs: 'cna'
     })
     .state('categoryStories', {
-      url: '/category/:name',  // this was "/:name" and that ALSO matches /about which is bad.
-      // templateUrl: ''  // a blank templateURL loads http://localhost:8080/ (which means he index.html file!)
+      url: '/category/:name',
     })
     .state('allStories', {
       url: '/allStories',
@@ -50,8 +48,6 @@
     })
     .state('post', {
       url: '/post',
-      // templateUrl: '',
-      // templateUrl: '',
       controller: 'CreatePostController',
       controllerAs: 'post'
       // TODO: create a template for "post" and include its URL here'
