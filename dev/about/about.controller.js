f52d0428de4c71a3d385696bb29f40669b6a7326
MJG <MJG@MacBook-Pro.local>
2016-04-28 23:09:20 -0400

    made a few small edits so that grunt watch would work

--- dev/about/about.controller.js
+++ dev/about/about.controller.js
@@ -1,15 +1,15 @@
 (function() {
-  'use strict';
-
-  angular
-    .module('blog')
-    .controller('AboutController', AboutController);
+  // 'use strict';
+  //
+  // angular
+  //   .module('blog')
+  //   .controller('AboutController', AboutController);
 
 // We many not need the below function and inject afterall
   // AboutController.$inject = ["$state"];
   //
   // function AboutController($state){
-  // 
+  //
   //
   // }
 }());
