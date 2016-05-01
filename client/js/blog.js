662493163baac85782db7ee8b59b636746b2628f
Alex Perkins <acperkins@gmail.com>
2016-05-01 11:19:27 -0400

    basic delete functionality working if you're logged in

--- client/js/blog.js
+++ client/js/blog.js
@@ -92,9 +92,9 @@
     angular.module('blog')
       .controller('AuthorController', AuthorController);
 
-      AuthorController.$inject = ['$stateParams', 'postListFactory'];
+      AuthorController.$inject = ['$stateParams', 'LoginService', 'postListFactory', 'deleteFactory'];
 
-      function AuthorController($stateParams, postListFactory) {
+      function AuthorController($stateParams, LoginService, postListFactory, deleteFactory) {
         console.log($stateParams.id);
         console.log("in AuthorController");
         var that = this;
@@ -105,7 +105,13 @@
             console.log(posts);
             that.allPosts = posts;
         });
-        // this.recentPosts = postListFactory.getAllPosts();
+
+        this.deletePost = function deletePost(postId) {
+          console.log(postId);
+          console.log(LoginService.getLoginData().id);
+          deleteFactory.deletePost(postId, LoginService.getLoginData().id);
+        };
+
 
       }
 
@@ -296,6 +302,38 @@
   }
 
 }());
+;(function() {
+    'use strict';
+
+    angular
+      .module('blog')
+      .factory('deleteFactory', deleteFactory);
+
+    deleteFactory.$inject = ['$http'];
+
+    function deleteFactory($http) {
+
+      var apiURL = 'https://tiy-blog-api.herokuapp.com/api';
+
+      return {
+        deletePost: deletePost
+      };
+
+      function deletePost(postId, token) {
+        return $http({
+          method: 'DELETE',
+          url: apiURL + '/Posts/' + postId,
+          headers: {
+            Authorization: token
+          }
+        }).then(function successGetAllCategories(response) {
+          return response;
+        });
+      }
+
+    }
+
+})();
 ;(function() {
     'use strict';
 
