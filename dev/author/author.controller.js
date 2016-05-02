86499e22f29a219c26586b54b3d2cbdd06440240
Alex Perkins <acperkins@gmail.com>
2016-05-01 20:37:27 -0400

    Delete button only shows up for logged in user's posts. Styling added to delete button.

--- dev/author/author.controller.js
+++ dev/author/author.controller.js
@@ -7,23 +7,29 @@
       AuthorController.$inject = ['$state', '$stateParams', 'LoginService', 'postListFactory', 'deleteFactory'];
 
       function AuthorController($state, $stateParams, LoginService, postListFactory, deleteFactory) {
-        console.log($stateParams.id);
-        console.log("in AuthorController");
+
         var that = this;
+        this.authorId = "";
+
+        if (LoginService.getLoginData()) {
+          this.authorId = LoginService.getLoginData().userId;
+        }
+        console.log('after if', this.authorId);
         this.allPosts = [];
 
         postListFactory.getPostsByAuthorID($stateParams.id)
           .then(function viewPosts(posts) {
-            console.log(posts);
             that.allPosts = posts;
         });
 
         this.deletePost = function deletePost(postId) {
-          console.log(postId);
-          console.log(LoginService.getLoginData().id);
           deleteFactory.deletePost(postId, LoginService.getLoginData().id)
             .then(function deleteSuccess() {
-              $state.go();
+              $state.transitionTo($state.current, $stateParams, {
+                reload: true,
+                inherit: false,
+                notify: true
+              });
             });
         };
 
