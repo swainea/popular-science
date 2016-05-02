d638101f3ca46c8a7ea7d0512a1d6894e6ecd701
Liz Swain <swainea@gmail.com>
2016-05-01 22:20:29 -0400

    housekeeping on messy code

--- dev/create-post/create-post.controller.js
+++ dev/create-post/create-post.controller.js
@@ -2,13 +2,14 @@
   'use strict';
 
   angular
-    .module('blog')
-    .controller('CreatePostController', CreatePostController);
+  .module('blog')
+  .controller('CreatePostController', CreatePostController);
 
   CreatePostController.$inject = ['$state', 'CreatePostService', 'postListFactory', 'LoginService'];
 
   function CreatePostController ($state, CreatePostService, postListFactory, LoginService){
-
+    this.categoryList = [];
+    var that = this;
     this.myCategory = {};
 
     this.blogPost = {
@@ -21,40 +22,31 @@
     this.newPost = function newPost (){
 
       this.blogPost.categoryId = this.myCategory.id;
-
-      console.log("blogPost is: ", this.blogPost);
+      // console.log("blogPost is: ", this.blogPost);
       if (this.blogPost.newCategory){
 
         CreatePostService.createCategory(this.blogPost.newCategory)
-          .then (function handleCatData(catData) { //not getting into here
-            console.log(catData);
-            that.blogPost.categoryId = catData.id;
-            CreatePostService.submitPost(that.blogPost, LoginService.getLoginData().id)
-              .then(function successHandler(newPost) {
-                console.log(newPost);
-                $state.go("viewPost", {id: newPost.id});
+        .then (function handleCatData(catData) { //not getting into here
+          console.log(catData); //currently undefined
+          that.blogPost.categoryId = catData.id;
+          CreatePostService.submitPost(that.blogPost, LoginService.getLoginData().id)
+          .then(function successHandler(newPost) {
+            $state.go("viewPost", {id: newPost.id});
           });
         });
+
       } else { // this works as expected
-      CreatePostService.submitPost(this.blogPost, LoginService.getLoginData().id)
+        CreatePostService.submitPost(this.blogPost, LoginService.getLoginData().id)
         .then(function successHandler(newPost) {
-          console.log(newPost);
           $state.go("viewPost", {id: newPost.id});
         });
-    }
-  };
-
-    this.categoryList = [];
-    var that = this;
+      }
+    };
 
     postListFactory.getAllCategories()
-      .then(function (categories){
+    .then(function (categories){
       that.categoryList = categories.data;
-      // that.myCategory = that.categoryList[0];
+    });
 
-      // console.log(categories.data);
-      // console.log('My Category', that.myCategory);
-      });
-
-}
+  }
 }());
--- dev/create-post/create-post.service.js
+++ dev/create-post/create-post.service.js
@@ -15,7 +15,6 @@
     };
 
     function submitPost (blogPost, authorization){
-      // console.log(blogPost);
       return $http ({
         method:'POST',
         url: "https://tiy-blog-api.herokuapp.com/api/Posts",
@@ -24,10 +23,9 @@
           Authorization: authorization
         }
       }).then (function onSuccess(response){
-        // console.log("inside of onSuccess function", response);
         return response.data;
       }, function error(response) {
-        // console.log(response);
+        console.log(response);
       }
     );
     }
@@ -42,10 +40,9 @@
           Authorization: authorization
         }
       }).then (function onSuccess(response){
-        // console.log("inside of second onSuccess function", response);
         return response.data;
       }, function error(response) {
-        // console.log(response);
+        console.log(response);
       });
     }
   }
